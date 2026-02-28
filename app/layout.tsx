409408eb4a682cbde3cbc3c2e1d9aaf19c47b1f9
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-02-28 15:50:29 +0100

    feat: add meta fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from "next";
+import type { Metadata, ResolvingMetadata } from "next";
 import { Nunito, Nunito_Sans} from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
+import { createClient } from "@/prismicio";
+import Link from "next/link";
 
 const nunito = Nunito({
   variable: "--font-nunito-sans",
@@ -13,12 +15,27 @@ const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Flowrise",
-  description: "Flowrise - The Flow Designer",
-};
+type Props = {
+  params: Promise<{ id: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+ 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle('settings')
+  return {
+    title: page.data.site_title || "Flowrise",
+    description: page.data.meta_description || "Flowrise - The Flow Designer",
+    openGraph: {
+      images: [page.data.og_image.url || ""],
+    },
+}
+}
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -26,8 +43,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
+        <header>
+          <nav>
+            <ul>
+              {/* {navigation.data.navigation.map((item, index) => (
+                <li key={index}>
+                  <Link href={item.link.url}>{item.label}</Link>
+                </li>
+              ))} */}
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
+      <footer>
+        <p>Flowrise - The Flow Designer</p>
+      </footer>
     </html>
   );
 }
