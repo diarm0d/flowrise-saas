b22c8421ca0a691d5b7f64fcd7d81f788b58c4ff
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-02-28 12:12:10 +0100

    init:

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./slices/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        body: ["var(--font-nunito-sans)", "sans-serif"],
+        display: ["var(--font-nunito)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
