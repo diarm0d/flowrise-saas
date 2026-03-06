a610a9517861987e6ae465e4947dc1158cb98d64
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2026-03-06 07:59:20 +0100

    feat: refactor header & footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,31 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Logo from "./Logo";
+import Bounded from "./Bounded";
 
 export default async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
   return (
-    <footer className="flex items-center justify-between">
+    <Bounded as="footer" className="py-4 md:py-6 lg:py-8">
+      <div className="flex sm:flex-row flex-col items-center justify-between gap-4">
         <Link href="/">
-            <p>{settings.data.site_title}</p>
+          <Logo />
         </Link>
-        <p>Copyright © {new Date().getFullYear()} {settings.data.site_title}</p>
+        <p className="text-xs">
+          Copyright © {new Date().getFullYear()} {settings.data.site_title}
+        </p>
         <ul className="flex items-center gap-4">
           {settings.data.navigation.map((item, index) => (
             <li key={index}>
-              <PrismicNextLink field={item.link}>{item.label}</PrismicNextLink>
+              <PrismicNextLink className="p-3" field={item.link}>
+                {item.label}
+              </PrismicNextLink>
             </li>
           ))}
         </ul>
-    </footer>
-  )
+      </div>
+    </Bounded>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,33 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Bounded from "./Bounded";
+import Logo from "./Logo";
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle("settings");
   return (
-    <header className="flex items-center justify-between">
+    <Bounded
+      as="header"
+      className="py-4 md:py-6 lg:py-8"
+    >
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
         <Link href="/">
-      <p>{settings.data.site_title}</p>
+          <Logo />
         </Link>
-      <nav>
-        <ul className="flex items-center gap-4">
-          {settings.data.navigation.map((item, index) => (
-            <li key={index}>
-              <PrismicNextLink field={item.link}>{item.label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  )
+        <nav>
+          <ul className="flex items-center gap-4">
+            {settings.data.navigation.map((item, index) => (
+              <li key={index}>
+                <PrismicNextLink className="p-3" field={item.link}>
+                  {item.label}
+                </PrismicNextLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Bounded>
+  );
 }
